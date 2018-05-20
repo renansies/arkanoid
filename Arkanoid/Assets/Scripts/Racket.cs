b60d47c16060310e35a46a1764f5e9ff94e06541
Renan Sies <renansies@gmail.com>
2018-05-20 13:38:33 -0300

    Cenário e racket

---
+++ Arkanoid/Assets/Scripts/Racket.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+	public float Speed = 150;
+
+	private void FixedUpdate ()
+	{
+		float h = Input.GetAxisRaw("Horizontal");
+		GetComponent<Rigidbody2D>().velocity = Vector2.right * h * Speed;
+	}
+}
